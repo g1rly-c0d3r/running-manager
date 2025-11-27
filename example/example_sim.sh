df35ff51e7117bf8d751d4bf641fa08bdff81e86
g1rly-c0d3r <girly_c0d3r@aol.com>
2025-11-27 15:16:46 -0600

    Devel: updated client script and example simulation

--- example/example_sim.sh
+++ example/example_sim.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 #RNMN threads = 2
 
+echo "$PWD"
+
 echo -n 'doing something . . . '
 sleep 3
 echo 'done'
 
 echo -n 'doing something long . . . '
-sleep 180
+sleep 60
 echo 'done'
